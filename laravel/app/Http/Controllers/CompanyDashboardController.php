503097c8e461ad929b1dc57bfa9ad3c7270a66ad
MarceloDiasSoaresJunior <mdsj@outlook.com>
2024-06-09 18:34:35 -0300

    ajustando home.blade para nao exibir infos que nao sejam referentes a empresa logada

--- laravel/app/Http/Controllers/CompanyDashboardController.php
+++ laravel/app/Http/Controllers/CompanyDashboardController.php
@@ -55,164 +55,190 @@ class CompanyDashboardController extends Controller
     }
 
     public function home(Request $req)
-    {
-        $commonDates = CommomDataService::getCommonDates($req);
-        $company = Auth::user()->company;
-        $company_id = $company->id;
+{
+    $commonDates = CommomDataService::getCommonDates($req);
+    $company = Auth::user()->company;
+    $company_id = $company->id;
 
-       
+    $funcoes = Funcao::where('company_id', $company_id)
+        ->where('status', 1)
+        ->whereHas('atividades', function ($queryAtividade) use ($commonDates, $company_id) {
+            $queryAtividade->where('company_id', $company_id)
+                ->whereHas('atividade_dias_semana', function ($queryAtividadeDia) use ($commonDates, $company_id) {
+                    $queryAtividadeDia->where('company_id', $company_id)
+                        ->where('dia_da_semana', $commonDates['dayOfTheWeek']);
+                });
+        })
+        ->limit(10)
+        ->orderBy('id', 'DESC')
+        ->get();
 
-        $funcoes = Funcao::where('company_id', $company_id)
-            ->where('status', 1)
-            ->whereHas('atividades', function ($queryAtividade) use ($commonDates, $company_id) {
-                $queryAtividade->where('company_id', $company_id)
-                    ->whereHas('atividade_dias_semana', function ($queryAtividadeDia) use ($commonDates, $company_id) {
-                        $queryAtividadeDia->where('company_id', $company_id)
-                            ->where('dia_da_semana', $commonDates['dayOfTheWeek']);
-                    });
-            })->limit(10)->orderBy('id', 'DESC')->get();
-
-        foreach ($funcoes as $funcao) {
-            $atividades = [];
-            foreach ($funcao->atividades as $atividade) {
-                foreach ($atividade->atividade_dias_semana as $dia) {
-                    if ($dia->dia_da_semana === $commonDates['dayOfTheWeek']) {
-                        array_push($atividades, [$atividade, $dia]);
-                    }
+    foreach ($funcoes as $funcao) {
+        $atividades = [];
+        foreach ($funcao->atividades as $atividade) {
+            foreach ($atividade->atividade_dias_semana as $dia) {
+                if ($dia->dia_da_semana === $commonDates['dayOfTheWeek'] && $dia->company_id == $company_id) {
+                    array_push($atividades, [$atividade, $dia]);
                 }
             }
+        }
 
-            $atividades_total_funcao_completadas = 0;
+        $atividades_total_funcao_completadas = 0;
+        $total_funcionarios = 0;
 
-            $total_funcionarios = 0;
+        foreach ($atividades as $at) {
+            $hasAtividade = FuncionarioAtividade::where('company_id', $company_id)
+                ->where('atividade_id', $at[0]->id)
+                ->where('dia', $commonDates['dateForMySQL'])
+                ->where('status', 1)
+                ->orderBy('id', 'DESC')
+                ->get();
 
-            foreach ($atividades as $at) {
-                $hasAtividade = FuncionarioAtividade::where('company_id', $company_id)
-                    ->where('atividade_id', $at[0]->id)
-                    ->where('dia', $commonDates['dateForMySQL'])
-                    ->where('status', 1)
-                    ->orderBy('id', 'DESC')
-                    ->get();
+            if ($hasAtividade->count()) {
+                $atividades_total_funcao_completadas += $hasAtividade->count();
+            }
 
-                if ($hasAtividade->count()) {
-                    $atividades_total_funcao_completadas = $atividades_total_funcao_completadas + $hasAtividade->count();
-                }
-
-            
-                $getFuncionarios = AtividadeFuncionario::where('funcionarios.company_id', $company_id)
-                ->join('funcionarios', 'atividade_funcionarios.funcionario_id', 'funcionarios.id')
+            $getFuncionarios = AtividadeFuncionario::where('company_id', $company_id)
                 ->where('atividade_id', $at[0]->id)
                 ->where('status', 1)
+                ->join('funcionarios', 'atividade_funcionarios.funcionario_id', '=', 'funcionarios.id')
+                ->where('funcionarios.company_id', $company_id)
                 ->groupBy('funcionarios.id')
                 ->count();
 
-
-                $total_funcionarios += $getFuncionarios;
-            }
-
-            $funcao->total_funcionarios = $total_funcionarios;
-            $funcao->porcentagem_completas = round($this->getPercentage($atividades_total_funcao_completadas, count($atividades)));
+            $total_funcionarios += $getFuncionarios;
         }
 
-        $funcionarios = Funcionario::where('company_id', $company_id)->limit(10)->orderBy('id', 'DESC')->get();
+        $funcao->total_funcionarios = $total_funcionarios;
+        $funcao->porcentagem_completas = round($this->getPercentage($atividades_total_funcao_completadas, count($atividades)));
+    }
 
-        foreach ($funcionarios as $funcionario) {
+    $funcionarios = Funcionario::where('company_id', $company_id)->limit(10)->orderBy('id', 'DESC')->get();
 
-            $atividadesCadastradas = AtividadeFuncionario::where('company_id', $company_id)
-                ->where('funcionario_id', $funcionario->id)
-                ->where('status', 1)
-                ->get()
-                ->count();
+    foreach ($funcionarios as $funcionario) {
+        $atividadesCadastradas = AtividadeFuncionario::where('company_id', $company_id)
+            ->where('funcionario_id', $funcionario->id)
+            ->where('status', 1)
+            ->get()
+            ->count();
 
-            $atividadesCompletas = FuncionarioAtividade::where('company_id', $company_id)
-                ->where('dia', $commonDates['dateForMySQL'])
-                ->where('funcionario_id', $funcionario->id)
-                ->where('status', 1)
-                ->get()
-                ->count();
+        $atividadesCompletas = FuncionarioAtividade::where('company_id', $company_id)
+            ->where('dia', $commonDates['dateForMySQL'])
+            ->where('funcionario_id', $funcionario->id)
+            ->where('status', 1)
+            ->get()
+            ->count();
 
-            $funcionario->atividades = $atividadesCadastradas;
+        $funcionario->atividades = $atividadesCadastradas;
+        $funcionario->porcentagem_completas = round($this->getPercentage($atividadesCompletas, $atividadesCompletas));
+    }
 
-            $funcionario->porcentagem_completas = round($this->getPercentage($atividadesCompletas, $atividadesCompletas));
-        }
+    $atividades = Atividade::select('atividades.*')
+        ->join('atividade_funcionarios', 'atividade_funcionarios.atividade_id', 'atividades.id')
+        ->where('atividades.company_id', $company_id)
+        ->limit(10)
+        ->orderBy('id', 'desc')
+        ->get();
 
-
-        $atividades = Atividade::select('atividades.*')
-            ->join('atividade_funcionarios', 'atividade_funcionarios.atividade_id', 'atividades.id')
-            ->limit(10)
+    foreach ($atividades as $row_atividade) {
+        $atividade_funcionario = AtividadeFuncionario::where('atividade_id', $row_atividade->id)
+            ->where('company_id', $company_id)
+            ->value('id');
+        $senderId = Observacao::select('sender_id')
+            ->where('atividade_funcionario_id', $atividade_funcionario)
+            ->where('company_id', $company_id)
             ->orderBy('id', 'desc')
-            ->get();
+            ->value('sender_id');
+        $row_atividade->atividade_funcionario = $atividade_funcionario;
+        $row_atividade->observacao = Observacao::select('message')
+            ->where('id', $senderId)
+            ->where('company_id', $company_id)
+            ->value('message');
+        $row_atividade->observacao_count = Observacao::select('message')
+            ->where('atividade_funcionario_id', $row_atividade->id)
+            ->where('company_id', $company_id)
+            ->count();
 
-        foreach($atividades as $row_atividade) {
-            $atividade_funcionario = AtividadeFuncionario::where('atividade_id', $row_atividade->id)->value('id');
-            $senderId = Observacao::select('sender_id')->where('atividade_funcionario_id', $atividade_funcionario)->orderBy('id', 'desc')->value('sender_id');
-            $row_atividade->atividade_funcionario = $atividade_funcionario;
-            $row_atividade->observacao = Observacao::select('message')->where('id', $senderId)->value('message');
-            $row_atividade->observacao_count = Observacao::select('message')->where('atividade_funcionario_id', $row_atividade->id)->count();
+        $funcionario_id = AtividadeFuncionario::where('atividade_id', $row_atividade->id)
+            ->where('company_id', $company_id)
+            ->value('funcionario_id');
+        $row_atividade->funcao_title = Funcao::where('id', $row_atividade->funcao_id)
+            ->where('company_id', $company_id)
+            ->value('title');
+        $row_atividade->funcionario_id = $funcionario_id;
+        $row_atividade->user_id = User::select('users.id')
+            ->join('funcionarios', 'users.id', 'funcionarios.user_id')
+            ->where('funcionarios.id', $funcionario_id)
+            ->where('funcionarios.company_id', $company_id)
+            ->value('users.id');
+        $row_atividade->user_name = User::select('name')
+            ->join('funcionarios', 'users.id', 'funcionarios.user_id')
+            ->where('funcionarios.id', $funcionario_id)
+            ->where('funcionarios.company_id', $company_id)
+            ->value('name');
 
-            $funcionario_id = AtividadeFuncionario::where('atividade_id', $row_atividade->id)->value('funcionario_id');
-            $row_atividade->funcao_title = Funcao::where('id', $row_atividade->funcao_id)->value('title');
-            $row_atividade->funcionario_id = $funcionario_id;
-            $row_atividade->user_id = User::select('users.id')->join('funcionarios', 'users.id', 'funcionarios.user_id')->where('funcionarios.id', $funcionario_id)->value('users.id');
-            $row_atividade->user_name = User::select('name')->join('funcionarios', 'users.id', 'funcionarios.user_id')->where('funcionarios.id', $funcionario_id)->value('name');
-
-            $row_atividade->funcionario_atividade = FuncionarioAtividade::select('status')
-            ->where('company_id', $company->id)
+        $row_atividade->funcionario_atividade = FuncionarioAtividade::select('status')
+            ->where('company_id', $company_id)
             ->where('atividade_id', $row_atividade->id)
             ->where('dia', $commonDates['dateForMySQL'])
             ->value('status');
-        }
-
-        $now = CommomDataService::getCarbonNow();
-
-        $frequencias = DB::table('funcionarios')
-                       ->select('frequencias.*', 'users.name', 'funcoes.title as funcao_title')
-                       ->leftjoin('frequencias', 'frequencias.funcionario_id', 'funcionarios.id')
-                       ->join('users', 'users.id', 'funcionarios.user_id')
-                       ->join('funcoes', 'funcoes.id', 'funcionarios.funcao_id')
-                       ->where('funcionarios.company_id', $company_id)
-                       ->limit(10)
-                       ->orderBy('id', 'desc')
-                       ->get();
-    
-        $atividades_detalhes = FuncionarioAtividade::where('company_id', $company_id)
-            ->whereDate('created_at', $now)
-            ->limit(10)
-            ->orderBy('id', 'DESC')
-            ->get();
-       
-
-
-        $funcoes_list = Funcao::where('company_id', $company_id)->where('status', 1)->limit(10)->orderBy('id', 'DESC')->get();
-       
-
-        $jornadas = Jornada::where('company_id', $company_id)->where('status', 1)->limit(10)->orderBy('id', 'DESC')->get();
-
-       
-
-        $funcionarios_list = Funcionario::where('company_id', $company_id)->limit(10)->orderBy('id', 'DESC')->get();
-
-       
-
-        $data = [
-            'historico_action' => '/',
-            'total_funcoes' => Funcao::where('company_id', $req->user()->company->id)->where('status', 1)->count(),
-            'total_jornadas' => Jornada::where('company_id', $req->user()->company->id)->where('status', 1)->count(),
-            'total_observacoes' => Observacao::where('company_id', $req->user()->company->id)->count(),
-            'total_atividades' => Atividade::where('company_id', $req->user()->company->id)->where('status', 1)->count(),
-            'total_funcionarios' => Funcionario::where('company_id', $req->user()->company->id)->count(),
-            'atividades' => $atividades,
-            'funcoes' => $funcoes,
-            'funcionarios' => $funcionarios,
-            'frequencias' => $frequencias,
-            'atividades_detalhes' => $atividades_detalhes,
-            'funcoes_list' => $funcoes_list,
-            'funcionarios_list' => $funcionarios_list,
-            'jornadas' => $jornadas,
-            ...$commonDates
-        ];
-        return view('pages.company.home', $data);
     }
+
+    $now = CommomDataService::getCarbonNow();
+
+    $frequencias = DB::table('funcionarios')
+        ->select('frequencias.*', 'users.name', 'funcoes.title as funcao_title')
+        ->leftJoin('frequencias', 'frequencias.funcionario_id', 'funcionarios.id')
+        ->join('users', 'users.id', 'funcionarios.user_id')
+        ->join('funcoes', 'funcoes.id', 'funcionarios.funcao_id')
+        ->where('funcionarios.company_id', $company_id)
+        ->limit(10)
+        ->orderBy('frequencias.id', 'desc')
+        ->get();
+
+    $atividades_detalhes = FuncionarioAtividade::where('company_id', $company_id)
+        ->whereDate('created_at', $now)
+        ->limit(10)
+        ->orderBy('id', 'DESC')
+        ->get();
+
+    $funcoes_list = Funcao::where('company_id', $company_id)
+        ->where('status', 1)
+        ->limit(10)
+        ->orderBy('id', 'DESC')
+        ->get();
+
+    $jornadas = Jornada::where('company_id', $company_id)
+        ->where('status', 1)
+        ->limit(10)
+        ->orderBy('id', 'DESC')
+        ->get();
+
+    $funcionarios_list = Funcionario::where('company_id', $company_id)
+        ->limit(10)
+        ->orderBy('id', 'DESC')
+        ->get();
+
+    $data = [
+        'historico_action' => '/',
+        'total_funcoes' => Funcao::where('company_id', $req->user()->company->id)->where('status', 1)->count(),
+        'total_jornadas' => Jornada::where('company_id', $req->user()->company->id)->where('status', 1)->count(),
+        'total_observacoes' => Observacao::where('company_id', $req->user()->company->id)->count(),
+        'total_atividades' => Atividade::where('company_id', $req->user()->company->id)->where('status', 1)->count(),
+        'total_funcionarios' => Funcionario::where('company_id', $req->user()->company->id)->count(),
+        'atividades' => $atividades,
+        'funcoes' => $funcoes,
+        'funcionarios' => $funcionarios,
+        'frequencias' => $frequencias,
+        'atividades_detalhes' => $atividades_detalhes,
+        'funcoes_list' => $funcoes_list,
+        'funcionarios_list' => $funcionarios_list,
+        'jornadas' => $jornadas,
+        ...$commonDates
+    ];
+    return view('pages.company.home', $data);
+}
+
 
     private function getPercentage($completas, $total)
     {

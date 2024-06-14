5a5baf44bf8ec204faeacbe86cdde2ab6944bdfd
MarceloDiasSoaresJunior <mdsj@outlook.com>
2024-06-14 18:03:38 -0300

    ajustando caminho dos certificados

--- flask/api.py
+++ flask/api.py
@@ -384,4 +384,5 @@ def register_ponto():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('/root/pointpro7/docker/nginx/cert.pem', '/root/pointpro7/docker/nginx/key.pem'))
+    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('/root/ssl/cert.pem', '/root/ssl/key.pem'))
+

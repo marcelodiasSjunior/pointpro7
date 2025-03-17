e4bc9d39d8548d748a04358a8bd775b04c0f4967
marcelo_dias_soares_jr <marcelo.dias@contabilizei.com.br>
2025-03-17 13:18:13 -0300

    ao editar nao manter obrigatoriedade de todos os campos

--- laravel/app/Http/Requests/JornadaUpdateRequest.php
+++ laravel/app/Http/Requests/JornadaUpdateRequest.php
@@ -22,14 +22,7 @@ class JornadaUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|string|max:255',
-            'segunda' => 'required|date_format:H:i',
-            'terca' => 'required|date_format:H:i',
-            'quarta' => 'required|date_format:H:i',
-            'quinta' => 'required|date_format:H:i',
-            'sexta' => 'required|date_format:H:i',
-            'sabado' => 'required|date_format:H:i',
-            'domingo' => 'required|date_format:H:i',
+            'title' => 'required|string|max:255'
         ];
     }
 }
